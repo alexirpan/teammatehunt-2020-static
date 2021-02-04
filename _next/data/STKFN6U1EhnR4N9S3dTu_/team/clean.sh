bffe2cbe2dd8f2dcdfb7933cb5c6a1b03df52fae
Alex Irpan <xelanapri@gmail.com>
2021-02-04 00:41:23 -0800

    Another export, broken teams should be fixed now.

---
+++ _next/data/STKFN6U1EhnR4N9S3dTu_/team/clean.sh
@@ -0,0 +1,14 @@
+mv "Moose Capers%2A.json" "Moose%20Capers*.json"
+mv "Have you tried our heated ivy%3F.json" "Have you tried our heated ivy?.json"
+mv "Just Zis Guy, K'know%3F.json" "Just Zis Guy, K'know?.json"
+mv "Track%3A Coconut Pizza Hut.json" "Track: Coconut Pizza Hut.json"
+mv "%22HEY LISTEN TO NAVI%22.json" '"HEY LISTEN TO NAVI".json'
+mv "Is Mayonnaise an Instrument%3F.json" "Is Mayonnaise an Instrument?.json"
+mv "ethan %3A).json" "ethan :).json"
+
+mkdir -p "Esoteric Coteries of /r"
+mv "Esoteric Coteries of %2Fr%2FPictureGame.json" "Esoteric Coteries of /r/PictureGame.json"
+mkdir -p "Erotic Cake of r/"
+mv "Erotic Cake of r%2FPictureGame.json" "Erotic Cake of r/PictureGame.json"
+mkdir -p "¯\_(ツ)_"
+mv "¯%5C_(ツ)_%2F¯.json" "¯\_(ツ)_/¯.json"
